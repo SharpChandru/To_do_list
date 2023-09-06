c8a079915d23cfea0f81af6906b63fa93f38b2f0
SharpChandru <gchandru2017@gmail.com>
2023-09-06 21:12:08 +0530

    to do list created

---
+++ src/Project.js
@@ -0,0 +1,24 @@
+// import { useState } from "react";
+
+
+// function Project(){
+//     const [Bcolor,setBcolor]=useState("empty value");
+//     const [Tcolor,setTcolor]=useState('');
+    
+
+//     function toggleColor(){
+//  const color={color:'black'?'white':'black'};
+
+//  setTcolor(color)
+//     }
+//     return (
+//         <>
+//         <div style={{backgroundColor:Bcolor,color:Tcolor.color}}>
+//         <h1 >{Bcolor}  </h1>
+//         </div>
+//         <input type="text" onChange={(e)=>{setBcolor(e.target.value);}} /><br /><br />
+//         <button type="button" onClick={()=>toggleColor()} >Toggle Text Color</button><br />
+//         </>
+//     )
+//     }
+// export default Project;
